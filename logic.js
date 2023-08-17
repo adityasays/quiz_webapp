247a4ed0740bd3ad4a249d1bcb991f979cf903f2
adityasays <aditya.shukla019r@gmail.com>
2023-08-17 22:06:47 +0530

    major changes completed

--- logic.js
+++ logic.js
@@ -5,24 +5,97 @@ let options = document.querySelectorAll(".option");
 let content=document.querySelector(".content")
 const questions = [
     {
-        question: "What is the full form of HTML",
+        question: "I speak without a mouth and hear without ears. I have no body, but I come alive with the wind. What am I?",
         answers: [
-            { text: "hamba tamba markup language", correct: false },
-            { text: "hit tati mar lang", correct: false },
-            { text: "random testing horhi", correct: false },
-            { text: "HYpertext markup language", correct: true }
+            { text: "Whistle", correct: false },
+            { text: "Echo", correct: true },
+            { text: "Ghost", correct: false },
+            { text: "Cloud", correct: false }
         ]
     },
     {
-        question: "terese puchi ?",
+        question: "You see a boat filled with people. It has not sunk, but when you look again you don’t see a single person on the boat. Why?",
         answers: [
-            { text: "haan", correct: false },
-            { text: "nah", correct: false },
-            { text: "hain", correct: false },
-            { text: "urey bhai", correct: true }
+            { text: "They jumped out", correct: false },
+            { text: "It's magic", correct: false },
+            { text: "They all drowned", correct: false },
+            { text: "All the people were married", correct: true }
+        ]
+    },
+    {
+        question: "What can you hold in your left hand but not in your right hand?",
+        answers: [
+            { text: "A pencil", correct: false },
+            { text: "Your right hand", correct: true },
+            { text: "A coin", correct: false },
+            { text: "A book", correct: false }
+        ]
+    },
+    {
+        question: "I am taken from a mine, and shut up in a wooden case, from which I am never released, and yet I am used by almost every person. What am I?",
+        answers: [
+            { text: "Coal", correct: false },
+            { text: "Water", correct: false },
+            { text: "Salt", correct: true },
+            { text: "Gold", correct: false }
+        ]
+    },
+    {
+        question: "The more you take, the more you leave behind. What am I?",
+        answers: [
+            { text: "Footsteps", correct: true },
+            { text: "Money", correct: false },
+            { text: "Memories", correct: false },
+            { text: "Work", correct: false }
+        ]
+    },
+    {
+        question: "What has keys but can't open locks?",
+        answers: [
+            { text: "Piano", correct: true },
+            { text: "Keyboard", correct: false },
+            { text: "Map", correct: false },
+            { text: "Locksmith", correct: false }
+        ]
+    },
+    {
+        question: "I am not alive, but I can grow; I don't have lungs, but I need air; I don't have a mouth, but water kills me. What am I?",
+        answers: [
+            { text: "Plant", correct: true },
+            { text: "Fish", correct: false },
+            { text: "Fire", correct: false },
+            { text: "Ice", correct: false }
+        ]
+    },
+    {
+        question: "What comes once in a minute, twice in a moment, but never in a thousand years?",
+        answers: [
+            { text: "Letter 'M'", correct: true },
+            { text: "Number 2", correct: false },
+            { text: "Rainbow", correct: false },
+            { text: "Leap year", correct: false }
+        ]
+    },
+    {
+        question: "I am always hungry, I must always be fed. The finger I touch will soon turn red. What am I?",
+        answers: [
+            { text: "Fire", correct: true },
+            { text: "Vampire", correct: false },
+            { text: "Monster", correct: false },
+            { text: "Tiger", correct: false }
+        ]
+    },
+    {
+        question: "You can drop me from the tallest building and I'll be fine, but if you drop me in water I die. What am I?",
+        answers: [
+            { text: "Feather", correct: true },
+            { text: "Stone", correct: false },
+            { text: "Glass", correct: false },
+            { text: "Metal", correct: false }
         ]
     }
 ];
+
 let questionnumber = 0;
 let score = 0;
 let correctans;
@@ -54,12 +127,17 @@ function nextbutton() {
     showques();
     
 }
-function showresult()
-{
-    content.innerHTML = "<h3 class='fin'>Your Final score is </h3>" + score + "<h3 class='fin'> out of </h3>" + questions.length;
+
+function showresult() {
+    content.innerHTML = `<h3 class='fin'>Your Final score is ${score} out of ${questions.length} </h3> `;
     btntext.innerText = "Replay";
-    btn.removeEventListener("click", nextbutton); // Removing the nextbutton listener
-    btn.addEventListener("click", startq);
+
+    btn.removeEventListener("click", nextbutton);
+    btn.addEventListener("click", reloadPage); 
+}
+
+function reloadPage() {
+    location.reload(); 
 }
 
 function checkans(e) {
@@ -83,7 +161,7 @@ function checkans(e) {
 }
 
 btn.addEventListener("click", () => {
-    if (questionnumber < questions.length ) {
+    if (questionnumber < questions.length-1 ) {
         nextbutton();
     } else {
         showresult()
